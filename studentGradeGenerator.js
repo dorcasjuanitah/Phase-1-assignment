90f4f1020431b3244807167cee12f3682e7f779c
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-03-24 21:19:21 +0300

    return grade

--- studentGradeGenerator.js
+++ studentGradeGenerator.js
@@ -2,14 +2,19 @@ let mark = document.querySelector("#mark").value;
 function grading(mark){
     if (mark >= 79 && mark === 100){
         grade = "A";
+        return grade;
     }
     else if(mark >=60 && mark < 80){
         grade = "B";
+        return grade;
     }
     else if(mark >=50 && mark <60){
-        grade = "C"
+        grade = "C";
+        return grade;
     }
     else{
         grade = "D";
+        return grade;
     }
 }
+console.log (grading(mark));
