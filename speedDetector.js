48e730b22954dd87aa915b79d93813e045555c00
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-03-24 21:11:26 +0300

    adds Speed generator code

---
+++ speedDetector.js
@@ -0,0 +1,21 @@
+let speed;
+let distance = 5;
+let dermit;
+
+function speedOfCar(){
+    if (speed <= 70){
+        console.log("OK");
+    }
+}
+
+function dermitCount(){
+    if (speed >=80 && distance === 5){
+        for (points =0; i<12; i++){
+            return points = 1;
+        }
+        console.log(`You have a total of over ${dermitCount()} `);
+    }
+    if (dermitCount === 12){
+        console.log("License Supended.");
+    }
+}
